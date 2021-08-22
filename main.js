24711bd2419e523e5229c09429313f502367efae
trudek <damian.trutkowski@gmail.com>
2021-08-22 12:27:04 +0200

    added menu

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-// comment
+// menu show - y hidden
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+//show menu
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+
+//hide menu
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
+
+//hide menu when selected
+function linkPressed() {
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show-menu');
+}
+const navLink = document.querySelectorAll('.nav__link');
+navLink.forEach((n) => n.addEventListener('click', linkPressed));
